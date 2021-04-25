0f4843c78952d4fe8d209691f0312e3f7f519c5f
Andrew <jobshniros@gmail.com>
2021-04-25 21:21:58 +0300

    add first user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  id:number;
+  name:Name;
+  contacts:Contacts;
+  tags:String[];
   constructor() { }
 
   ngOnInit(): void {
+    this.id = 1;
+    this.name = {
+      first_name:'Andrew',
+      last_name:'Ovchinnikov'
+  };
+    this.contacts = {
+      phone_number:'88005535500',
+      e_mail:'jobshniros@gmail.com'
+    };
+    this.tags = ["Fatman","Playboy","Gamer"];
   }
 
+}
+interface Name {
+  first_name:String;
+  last_name:String;
+}
+interface Contacts {
+  phone_number:String;
+  e_mail:String;
 }
